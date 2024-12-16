b81ec42ae12e0867d841efe5c840a3c77bcfec95
Kyle Bernhardy <kyle@harperdb.io>
2024-12-16 15:07:13 -0700

    destructuring from process.env better error message for validation added try / catch around getting a secret from the SECRET_LIST delete env variables specific to accessing the vault. updated the README.md

--- README.md
+++ README.md
@@ -2,15 +2,30 @@
 This project accesses an Azure key vault and assigns the keys to environment variables.
 
 This project executes as a dependency in HarperDB component and needs to be the first dependency to load in order to ensure the environment variables are loaded prior to being accessed.
+## Installation
+Go into the HarperDB application you are building and install this package and add it to the config.yaml file:
 
-## Add dependecy info
+1. Install:
+`npm install --save @harperdb/azure-secrets-to-environment`
 
-## Add config.yaml info
+2. Add to config.yaml. `@harperdb/azure-secrets-to-environment` must be the first dependency listed:
+```
+'@harperdb/azure-secrets-to-environment':
+  package: '@harperdb/azure-secrets-to-environment'
+'@harperdb/http-router':
+  package: '@harperdb/http-router'
+  files: '*.*js' # to load the routes.js and config files
+'@harperdb/nextjs':
+  package: '@harperdb/nextjs'
+  files: '/*'
+```
+
 
 ## Environment variables
 This project relies on environment variables to properly access the desired Azure key vault.  Please note this project relies on an Azure application registration for credentials.
 
-* AZURE_VAULT_NAME - Name of the Azure Key Vault holding the secrets
-* AZURE_TENANT_ID - Tenant ID of the Azure Subscription
-* AZURE_CLIENT_ID - Client ID of the application registration
-* AZURE_CLIENT_SECRET - Client Secret of the application registration
+* AZURE_VAULT_NAME - (required) Name of the Azure Key Vault holding the secrets
+* AZURE_TENANT_ID - (required) Tenant ID of the Azure Subscription
+* AZURE_CLIENT_ID - (required) Client ID of the application registration
+* AZURE_CLIENT_SECRET - (required) Client Secret of the application registration
+* SECRETS_LIST - (optional) Comma seperated list of secrets to access from the vault
--- extension.js
+++ extension.js
@@ -2,30 +2,28 @@ import { isMainThread } from 'node:worker_threads';
 import { ClientSecretCredential } from "@azure/identity";
 import { SecretClient } from "@azure/keyvault-secrets";
 
-const AZURE_VAULT_NAME = process.env.AZURE_VAULT_NAME;
-const AZURE_TENANT_ID = process.env.AZURE_TENANT_ID;
-const AZURE_CLIENT_ID = process.env.AZURE_CLIENT_ID;
-const AZURE_CLIENT_SECRET = process.env.AZURE_CLIENT_SECRET;
+const { AZURE_VAULT_NAME, AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET } = process.env;
 //comma seperated list of secrets
 const SECRETS_LIST = process.env.SECRETS_LIST?.split(',');
 
+const ERROR_PREAMBLE = 'Unable to access Azure Secrets Vault due to: ';
+
 if(isMainThread) {
-  try {
     //validate that AZURE_VAULT_NAME, AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET are defined.
     if (!AZURE_VAULT_NAME) {
-      throw new Error('AZURE_VAULT_NAME is required.');
+      throw new Error(`${ERROR_PREAMBLE} AZURE_VAULT_NAME is required.`);
     }
 
     if (!AZURE_TENANT_ID) {
-      throw new Error('AZURE_TENANT_ID is required.');
+      throw new Error(`${ERROR_PREAMBLE} AZURE_TENANT_ID is required.`);
     }
 
     if (!AZURE_CLIENT_ID) {
-      throw new Error('AZURE_CLIENT_ID is required.');
+      throw new Error(`${ERROR_PREAMBLE} AZURE_CLIENT_ID is required.`);
     }
 
     if (!AZURE_CLIENT_SECRET) {
-      throw new Error('AZURE_CLIENT_SECRET is required.');
+      throw new Error(`${ERROR_PREAMBLE} AZURE_CLIENT_SECRET is required.`);
     }
 
     // Configure vault URL
@@ -43,9 +41,13 @@ if(isMainThread) {
 
     if(SECRETS_LIST?.length > 0) {
       for (const secret_name of SECRETS_LIST) {
-        let secret = await client.getSecret(secret_name);
-        //set secret to process.env
-        process.env[secret.name] = secret.value;
+        try {
+          let secret = await client.getSecret(secret_name);
+          //set secret to process.env
+          process.env[secret.name] = secret.value;
+        } catch (error) {
+          console.warn(error.message);
+        }
       }
     } else {
       //if there is no predefined list of secrets iterate all secrets in vault
@@ -55,7 +57,10 @@ if(isMainThread) {
         process.env[secret.name] = secret.value;
       }
     }
-  } catch (error) {
-    console.error(`Unable to access Azure Secrets Vault due to: ${error.message}`);
-  }
+
+    //delete from environment so these keys are no longer accessible
+    delete process.env.AZURE_VAULT_NAME;
+    delete process.env.AZURE_TENANT_ID;
+    delete process.env.AZURE_CLIENT_ID;
+    delete process.env.AZURE_CLIENT_SECRET;
 }
