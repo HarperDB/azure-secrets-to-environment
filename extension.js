b3510b05eda497c0ae6e8fec0b9b6020b596cc92
Kyle Bernhardy <kyle@harperdb.io>
2024-12-22 16:29:54 -0700

    Added the abilitiy to output the env vars.  This is helpful for GH Actions

--- extension.js
+++ extension.js
@@ -2,7 +2,7 @@ import { ClientSecretCredential } from "@azure/identity";
 import { SecretClient } from "@azure/keyvault-secrets";
 import { appendFile, writeFile } from "node:fs/promises"
 
-const { AZURE_VAULT_NAME, AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, SAVE_ENV } = process.env;
+const { AZURE_VAULT_NAME, AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, PRINT_ENV } = process.env;
 //comma seperated list of secrets
 const SECRETS_LIST = process.env.SECRETS_LIST?.split(',');
 
@@ -38,30 +38,31 @@ const credential = new ClientSecretCredential(
 // Create authenticated secret client
 const client = new SecretClient(vaultUrl, credential);
 
+let envVars = {};
+
 if(SECRETS_LIST?.length > 0) {
   for (const secret_name of SECRETS_LIST) {
     try {
       let secret = await client.getSecret(secret_name);
       //set secret to process.env.  Because Azure KV does not support underscores, we put the secret names with dashes.  On retrieval we replace dashes with underscore
-      process.env[secret.name.replace(/-/g, '_')] = secret.value;
+      const secretName = [secret.name.replace(/-/g, '_')];
+      process.env[secretName] = secret.value;
+      envVars[secretName] = secret.value;
     } catch (error) {
       console.warn(error.message);
     }
   }
 } else {
-  if(SAVE_ENV === "true") {
-    await writeFile(process.cwd() + '/.env', '', { flags: 'a' });
-  }
-
   //if there is no predefined list of secrets iterate all secrets in vault
   for await (let secretProperties of client.listPropertiesOfSecrets()) {
     let secret = await client.getSecret(secretProperties.name);
     //set secret to process.env. Because Azure KV does not support underscores, we put the secret names with dashes.  On retrieval we replace dashes with underscore
     const secretName = [secret.name.replace(/-/g, '_')];
     process.env[secretName] = secret.value;
-
-    if(SAVE_ENV === "true") {
-      await appendFile(process.cwd() + '/.env', `${secretName}=${secret.value}\n`, { flags: 'a' });
-    }
+    envVars[secretName] = secret.value;
   }
+}
+
+if(PRINT_ENV) {
+  console.log(envVars);
 }
